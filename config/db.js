16c8a9e5c59fcd88354bdc3860515f337726091c
rahat <rahat.forja@gmail.com>
2017-12-20 06:14:42 -0600

    build bommit

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
 module.exports = {
-    database: '',
+    database: 'mongodb://dan:Chyea10!@' +
+        'cluster0-shard-00-00-h9ovu.mongodb.net:27017,' +
+        'cluster0-shard-00-01-h9ovu.mongodb.net:27017,' +
+        'cluster0-shard-00-02-h9ovu.mongodb.net:27017/blog?' +
+        'ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
     secret: '$%^USCMCITG&)*'
 }
